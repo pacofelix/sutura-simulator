a80e1ddbdc9178dd886b5cbd69d65b4f3dfacede
Francisco Félix <francisco.fgalvan@gmail.com>
2026-08-26 16:36:45 -0600

    Initial plugin structure

---
+++ sutura-simulator.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Simulador de Sutura
+ * Plugin URI: https://github.com/pacofelix/sutura-simulator
+ * Description: Plugin para la carga, procesamiento y retroalimentación de prácticas de sutura.
+ * Version: 0.1.0
+ * Author: Francisco Félix
+ * Text Domain: sutura-simulator
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
